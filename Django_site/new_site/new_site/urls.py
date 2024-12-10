7818af7be3f2869d4e4d31a86fb1767db16973cc
krisdikachi <krisdikachi@gmail.com>
2024-12-09 16:12:56 -0800

    second pushing

--- Django_site/new_site/new_site/urls.py
+++ Django_site/new_site/new_site/urls.py
@@ -21,7 +21,7 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('catalog/', include('catalog.urls')),
+
      path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 
